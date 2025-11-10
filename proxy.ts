d9d9ad7c22578d2ff02dae89f26034c7f88d2353
Syed Adeeb <abidi9897401278@gmail.com>
2025-11-11 04:07:55 +0530

    feat: update publicPaths in middleware to include additional routes for authentication

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,16 @@ import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 
 export default withAuth(async function middleware(req: Request) {}, {
 	// Middleware still runs on all routes, but doesn't protect the blog route
-	publicPaths: ["/"],
+	publicPaths: [
+		"/",
+		"/store",
+		"/store/:path*",
+		"/products/:path*",
+		"/collections/:path*",
+		"/about",
+		"/contact",
+		"/api/webhooks/kinde",
+	],
 });
 
 export const config = {
